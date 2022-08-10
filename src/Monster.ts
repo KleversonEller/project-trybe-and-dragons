008a839326adeca4133d821400ab18387ed477b3
Kleverson Eller <kleverson.a16@gmail.com>
2022-08-10 01:40:26 -0300

    feat: :sparkles: Criando class (Monster)
    Criando a class (Mosnter) responsavel por criar um novo mosntro

---
+++ src/Monster.ts
@@ -0,0 +1,24 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints() { return this._lifePoints; }
+  get strength() { return this._strength; }
+
+  attack(value: SimpleFighter) {
+    value.receiveDamage(this.strength);
+  }
+
+  receiveDamage(value: number) {
+    if (value > 0) { this._lifePoints -= value; }
+    if (this._lifePoints <= 0) { this._lifePoints = -1; }
+    return this.lifePoints;
+  }
+}
