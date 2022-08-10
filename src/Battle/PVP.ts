2134c4603657473cf9aa5c9e706b6487a13851da
Kleverson Eller <kleverson.a16@gmail.com>
2022-08-10 02:32:09 -0300

    feat: :sparkles: Criando class (PVP)
    Criando class (PVP) responsavel pelo duelo ate a morte entre dois  players

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,21 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  private player1: Fighter;
+  private player2: Fighter;
+
+  constructor(player1: Fighter, player2: Fighter) {
+    super(player1);
+    this.player1 = player1;
+    this.player2 = player2;
+  }
+
+  fight() {
+    while (this.player1.lifePoints !== -1 && this.player2.lifePoints !== -1) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
+    }
+    return super.fight();
+  }
+}
